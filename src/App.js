703549fadea97014db62b77d86bdcb8f5fa83e59
Poudlardo <55178305+Poudlardo@users.noreply.github.com>
2023-06-01 20:28:48 +0200

    create pages + navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Games from './components/pages/Games';
+import Blog from './components/pages/Blog';
+import About from './components/pages/About';
+import Connect from './components/pages/Connect';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/games' element={<Games />}/>
+          <Route path='/blog' element={<Blog />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/connect' element={<Connect />}/>
+        </Routes>
+    </Router>
+
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,46 @@
-import React, {useState} from 'react'
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
+
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <> 
     <nav className='navbar'>
         <div className='navbar-container'>
-            <Link to="/" className='navbar-logo'>
-                DEDRA
+            <Link to="/" className='navbar-logo' onClick={closeMobileMenu}>
+                DEDRA <i></i>
             </Link>
+            <ul>
+              <li>
+                <Link to="/" onClick={handleClick}>
+                    { click ? 'HOME' : <Link to="/games" onClick={closeMobileMenu}>GAMES</Link> }
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" onClick={closeMobileMenu}>
+                    ABOUT
+                </Link>
+              </li>
+              <li>
+                <Link to="/blog" onClick={closeMobileMenu}>
+                    BLOG
+                </Link>
+              </li>
+              <li>
+              <Link to="/connect" onClick={closeMobileMenu}>
+                  CONNECT
+              </Link>
+              </li>
+
+            </ul>
+
+            <div className='menu-icon'>
+            </div>
         </div>
     </nav>
     </>
