f221185a8b8a9ad2546171908526c7a0478e28c7
Poudlardo <55178305+Poudlardo@users.noreply.github.com>
2023-06-08 18:40:38 +0200

    start games page

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -1,9 +1,15 @@
 import React from "react";
 import '../App.css'
+import homeVideo from '../videos/GamesDedra-1438626014704308233-01.mp4';
 
 function Games() {
     return(
         <div className="gamespage">
+        <div className="videocontainer">
+                <video className="video-gamespage" autoPlay loop muted>
+        <source src={homeVideo} type="video/mp4" />
+                </video> 
+            </div>    
         </div>
     )
 }
