eddf53b10b23b6dc0f0d4570a97b153c42c52298
poudlardo <adamtbatou@free.fr>
2023-06-10 21:23:46 +0200

    implement vertical carousel on games page

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -1,15 +1,61 @@
-import React from "react";
-import '../App.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
 import homeVideo from '../videos/GamesDedra-1438626014704308233-01.mp4';
+import whiteDedraLogo from '../images/unnamed-trnsprntwhite.png';
+import whiteHbrgerLogo from '../images/white_hamburger-menu-svgrepo-com.svg';
+import data from "../data.json";
+import VerticalCarousel from "../components/VerticalCarousel";
+
 
 function Games() {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+  
+  function clickHamberger() {
+    const Links = document.querySelector('ul');
+    Links.classList.toggle('mobile-menu');
+  }
+
     return(
         <div className="gamespage">
-        <div className="videocontainer">
-                <video className="video-gamespage" autoPlay loop muted>
-        <source src={homeVideo} type="video/mp4" />
-                </video> 
-            </div>    
+                <video className="gamespage-video" autoPlay loop muted>
+                    <source src={homeVideo} type="video/mp4" />
+                </video>    
+                <div className='blacksail'>
+                </div> 
+                <nav className='gamespage-navbar'>
+            <Link to="/" className='dedra' onClick={closeMobileMenu}>
+            <img className="dedra-logo" src={whiteDedraLogo} /> DEDRA 
+            </Link>
+            <ul>
+            <li>
+                <Link className='nav-link' to="/" onClick={handleClick}>
+                    HOME
+                </Link>
+            </li>
+            <li>
+                <Link className='nav-link' to="/about" onClick={closeMobileMenu}>
+                    ABOUT
+                </Link>
+            </li>
+            <li>
+                <Link className='nav-link' to="/blog" onClick={closeMobileMenu}>
+                    BLOG
+                </Link>
+            </li>
+            <li>
+            <Link className='nav-link' to="/connect" onClick={closeMobileMenu}>
+                CONNECT
+            </Link>
+            </li>
+            </ul>
+            <img src={whiteHbrgerLogo} className='whiteHbrgerLogo' onClick={clickHamberger} />
+                </nav>      
+                <VerticalCarousel data={data.slides} leadingText={data.leadingText} />
         </div>
     )
 }
