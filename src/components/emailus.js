97cc1630ef02503cb3573ab9dde72b12c82ba055
Poudlardo <55178305+Poudlardo@users.noreply.github.com>
2023-06-29 21:25:22 +0200

    style connect page

---
+++ src/components/emailus.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './emailus.css';
+
+
+function Emailus() {
+
+  return (
+    <div class="svg-wrapper">
+        <svg height="60" width="320" xmlns="http://www.w3.org/2000/svg">
+        <rect class="shape" height="60" width="320" />
+        <div class="text">EMAIL US</div>
+        </svg>
+    </div>
+  )
+}
+
+export default Emailus
