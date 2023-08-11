ba4dbab141feb7defc0b8784c6667b3ee8c30916
Poudlardo <55178305+Poudlardo@users.noreply.github.com>
2023-08-11 19:54:42 +0200

    add connectPage prop + delete connectFooter component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,36 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
-function Footer() {
+function Footer({ connectPage = false }) {
+
+
   return (
     <div className="footer">
       <p className="leftpart">Dedra Games © 2023. All Rights Reserved.</p>
       <div className="rightpart">
-        <p className="follow">FOLLOW US</p>
+        { connectPage ?  
+          <Link className="cn-nav-link" to="/Privacypolicy">
+            PRIVACY POLICY
+          </Link> : <p className="follow">FOLLOW US</p>
+      }      
         <div className="line"></div>
-        <a href="https://discord.com/invite/ovo-speedrunning-community-645935373368688650">
-          <img src="images/discord.svg" className="socialmedias" />
-        </a>
-        <a href="https://www.youtube.com/channel/UCQ2CAlP8Zo8-P4I7hZ23Yfg">
-          <img src="images/youtube.svg" className="socialmedias" />
-        </a>
-        <a href="https://twitter.com/GamesDedra">
-          <img src="images/twitter.svg" className="socialmedias" />
-        </a>
+        { connectPage ? 
+           <Link className="cn-nav-link" to="/Termsofservice">
+                 TERMS OF SERVICE
+           </Link> :    
+           <div className="socialmedias">
+              <a href="https://discord.com/invite/ovo-speedrunning-community-645935373368688650">
+                <img src="images/discord.svg"  />
+              </a>
+              <a href="https://www.youtube.com/channel/UCQ2CAlP8Zo8-P4I7hZ23Yfg">
+                <img src="images/youtube.svg"/>
+              </a>
+              <a href="https://twitter.com/GamesDedra">
+                <img src="images/twitter.svg" />
+              </a>
+           </div>
+        }
       </div>
     </div>
   );
